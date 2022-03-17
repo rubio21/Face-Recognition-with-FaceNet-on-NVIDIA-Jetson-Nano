b29debb2ac9990776dd97101d25ff8db5765e62f
Alberto Rubio <92673739+rubio21@users.noreply.github.com>
2022-03-17 13:19:02 +0100

    take a photo added

--- src/interface.py
+++ src/interface.py
@@ -7,34 +7,34 @@ import time
 import face_recognition
 
 
-logos = [
-    [sg.Image(filename="../logos/cvc.png")],
-    [sg.Image(filename="../logos/uab.png")],
-]
+# logos = [
+#     [sg.Image(filename="../logos/cvc.png")],
+#     [sg.Image(filename="../logos/uab.png")],
+# ]
+sg.theme('Default1')
 
-boton=[
-    [sg.Button("Start system")],
-]
 lista = [
     [sg.Text("Employees in:")],
     [sg.Listbox(values=["Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5"], size=(40, 20), key="-LIST-")],
 ]
-prueba= [
-    [sg.Image(key="-IMAGE-")],
-]
+
 layout = [
     [
-        [sg.Text('Face Recognition System', key="-TITLE-", justification='center', font='Helvetica 20', background_color='#EE9444')]
+        [sg.Text('Face Recognition System', justification='center', font='Helvetica 20')]
     ],
     [
-        sg.Column(logos),
+        sg.Column([[sg.Image(key="-STREAM-")],]),
         sg.VSeparator(),
-        sg.Column(boton, key='-BUTTON-'),
-        sg.Column(lista, key='-HOME-', visible=False),
-        sg.Column(prueba, key='-RECOGNITION-', visible=False),
+        sg.Column(lista, key='-HOME-'),
+        sg.Column([
+            [sg.Image(key="-IMAGE-")],
+            [sg.Text("Welcome to CVC", key="-WELCOME-", justification='center', font='Helvetica 20')],
+        ], key='-RECOGNITION-', visible=False),
+
     ],
     [
-        [sg.Text("Welcome to CVC", key="-WELCOME-", justification='center', font='Helvetica 20', background_color='#EE9444')],
+        sg.Button("Take a photo"),
+        sg.Button("Exit")
     ]
 ]
 
@@ -45,35 +45,41 @@ embeddings = fr.load_face_embeddings("../Dataset/", fd)
 cap = cv2.VideoCapture(0)
 
 c=0
-window = sg.Window("Face Recognition", layout, resizable=True, background_color='#B0B0B0')
+window = sg.Window("Face Recognition", layout, resizable=True)
+control=False
+
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=0)
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    if event == "Start system":
-        window['-BUTTON-'].update(visible=False)
-        window['-HOME-'].update(visible=True)
 
-        while True:
-            ret, image = cap.read()
-            image = cv2.flip(image, 1)
-            reconocimiento = face_recognition.face_recognition(image, embeddings, fd, fr, False)
-            print(reconocimiento)
-            if reconocimiento != []:
-                image = Image.open("../Dataset/" + reconocimiento[0] + ".JPG")
-                image.thumbnail((400, 400))
-                bio = io.BytesIO()
-                image.save(bio, format="PNG")
-                window["-IMAGE-"].update(data=bio.getvalue())
-                window["-WELCOME-"].update("Welcome " + reconocimiento[0])
-                window['-HOME-'].update(visible=False)
-                window['-RECOGNITION-'].update(visible=True)
-                c = 0
-            else:
-                c += 1
-                if c == 25:
-                    window["-WELCOME-"].update("Welcome to CVC")
-                    window['-RECOGNITION-'].update(visible=False)
-                    window['-HOME-'].update(visible=True)
-            window.refresh()
+    ret, frame = cap.read()
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    window['-STREAM-'].update(data=imgbytes)
+    frame = cv2.flip(frame, 1)
+    reconocimiento = face_recognition.face_recognition(frame, embeddings, fd, fr, False)
+    print(reconocimiento)
+    if reconocimiento != []:
+        image = Image.open("../Dataset/" + reconocimiento[0] + ".JPG")
+        image.thumbnail((400, 400))
+        bio = io.BytesIO()
+        image.save(bio, format="PNG")
+        window["-IMAGE-"].update(data=bio.getvalue())
+        window["-WELCOME-"].update("Welcome " + reconocimiento[0])
+        window['-HOME-'].update(visible=False)
+        window['-RECOGNITION-'].update(visible=True)
+        c = 0
+    else:
+        c += 1
+        if c == 25:
+            window["-WELCOME-"].update("Welcome to CVC")
+            window['-RECOGNITION-'].update(visible=False)
+            window['-HOME-'].update(visible=True)
+
+    if event == "Take a photo":
+        persona = sg.popup_get_text(title='Save photo to database', message="Enter your name")
+        if persona != None:
+            cv2.imwrite("../Dataset/" + persona + "_1.jpg", frame)
+            sg.popup('Image saved!', title='Save photo to database', image=sg.EMOJI_BASE64_HAPPY_THUMBS_UP)
+
 window.close()
